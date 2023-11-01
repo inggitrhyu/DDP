53a7c93c938a4617d567b1a687a5d589f01fe01f
inggitrhyu <146051110+inggitrhyu@users.noreply.github.com>
2023-11-01 14:45:57 +0700

    Create Tugas5.1.py

---
+++ Pekan5/Tugas5.1.py
@@ -0,0 +1,10 @@
+#soal nomor 1 variabel kendaraan 
+kendaraan=["Beat Street","motor","110cc","Hitam",2]
+print(kendaraan)
+
+kendaraan.append(17000000)
+kendaraan.append("CBS")
+print(kendaraan)
+
+kendaraan.insert(2,"Honda")
+print(kendaraan)
